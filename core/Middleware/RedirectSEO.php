d0d949baea68c623f97c282aea16759a3b72bf84
padigiants <pa@digiants.com.ua>
2021-10-22 09:58:50 +0300

    fix Redirect SEO

--- core/Middleware/RedirectSEO.php
+++ core/Middleware/RedirectSEO.php
@@ -11,6 +11,9 @@ class RedirectSEO
     {
         $domain = parse_url(env('APP_URL'), PHP_URL_HOST);
         
+        if (!isset($_SERVER['HTTP_HOST']))
+            return $next($request);
+
         $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
         $filtered_url = preg_replace('/\/+/', '/', $url);
