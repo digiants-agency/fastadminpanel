a698deb9d5edaf603aa05ae81d56bb0ba0f18ee0
padigiants <pa@digiants.com.ua>
2022-02-01 14:33:45 +0200

    add 40101 SET to db

--- templates/shop/DTG/db.sql
+++ templates/shop/DTG/db.sql
@@ -1,6 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Час створення: Лют 01 2022 р., 13:18
+-- Версія сервера: 10.4.14-MariaDB
+-- Версія PHP: 7.4.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База даних: `shop`
