84bd103450df79f4c54bc1952c1c45079870de18
padigiants <pa@digiants.com.ua>
2022-02-16 16:16:42 +0200

    fix convertor 500

--- core/Middleware/Convertor.php
+++ core/Middleware/Convertor.php
@@ -7,11 +7,14 @@ class Convertor
     public function handle($request, $next, $guard = null)
     {	
         $response = $next($request);
+        
+        if ($response->status() !== 500) {
 
-        $response->setContent(
-            str_replace('%convertor%', \App\FastAdminPanel\Helpers\Convertor::convert(), $response->content())
-        );
-
+            $response->setContent(
+                str_replace('%convertor%', \App\FastAdminPanel\Helpers\Convertor::convert(), $response->content())
+            );        
+        }
+        
         return $response;
     }
 }
