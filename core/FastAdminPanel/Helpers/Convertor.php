7f83d7bbf8152be8e8e028db8637b173f2a05fab
Анатолий Проботюк <77732628+padigiants@users.noreply.github.com>
2023-06-01 01:44:59 +0300

    Update Convertor

--- core/FastAdminPanel/Helpers/Convertor.php
+++ core/FastAdminPanel/Helpers/Convertor.php
@@ -2,10 +2,6 @@
 
 namespace App\FastAdminPanel\Helpers;
 
-use App\FastAdminPanel\Helpers\Platform;
-use Illuminate\Http\Response as Response;
-use Route;
-
 class Convertor
 {
     private static $width = [
@@ -20,26 +16,30 @@ class Convertor
     
     public static $views = [];
 
-    public static function create($view, $style, $is_desktop = false)
-	{
-        if ($is_desktop) {
-
+    public static function create ($view, $style, $isDesktop = false)
+    {            
+        if ($isDesktop) {
             self::$views['desktop'][$view] = $style;
-
         } else {
-
             self::$views['mobile'][$view] = $style;
         }
     }
 
     public static function convert()
-	{
-        $cache_dir = public_path()."/css/cache";
-        $css_dir = "/css/cache";
+    {
+        // TODO: need collision research
+        $hash = hash('md5', implode('', array_keys(self::$views['desktop'] ?? [])));
+
+        $cacheDir = public_path()."/css/cache";
+        $cssDir = "/css/cache";
         
-        $components_dir = resource_path().'/views/';
-        
-        if (!empty(request()->route()->getAction()['controller'])) {
+        $componentsDir = resource_path().'/views/';
+
+        if (!empty(request()->attributes->get('controllerName'))) {
+            
+            $method = request()->attributes->get('controllerName');
+
+        } elseif (!empty(request()->route()->getAction()['controller'])) {
 
             $method = request()->route()->getAction()['controller'];
 
@@ -48,7 +48,7 @@ class Convertor
             $method = '404';
         }
 
-        $page_name = strtolower(
+        $pageName = strtolower(
             str_replace(
                 [
                     'App\Http\Controllers\\', 
@@ -64,96 +64,113 @@ class Convertor
             )
         );
 
+        $pageCss = $cacheDir.'/'.$pageName.$hash.'.css';
 
-        $page_css = $cache_dir.'/'.$page_name.'.css';
+        $isCache = true;
 
-        $is_cache = true;
+        $cacheTime = filemtime(self::$style_source_path['desktop']);
 
-        if (file_exists($page_css)) {
+        if (file_exists($pageCss)) {
 
-            // TODO: need collision research
             $base_time = filemtime(self::$style_source_path['desktop']);
-            $cache_time = $base_time;
+            $cacheTime = $base_time;
 
-            $cache_time += $base_time - filemtime(self::$style_source_path['mobile']);
+            $cacheTime += $base_time - filemtime(self::$style_source_path['mobile']);
 
-            foreach (self::$views['desktop'] as $key => $view){
+            if (isset(self::$views['desktop'])) {
 
-                $key = str_replace('.', '/', $key);
+                foreach (self::$views['desktop'] as $key => $view){
 
-                $cache_time += $base_time - filemtime($components_dir.$key.'.blade.php');
+                    $key = str_replace('.', '/', $key);
+    
+                    $cacheTime += $base_time - filemtime($componentsDir.$key.'.blade.php');
+                }
             }
 
-            $time_source = filemtime($page_css);
+            $time_source = filemtime($pageCss);
 
-            if ($time_source != $cache_time) {
+            if ($time_source != $cacheTime) {
 
-                $is_cache = false;
+                $isCache = false;
             }
 
         } else {
 
-            $is_cache = false;
+            $isCache = false;
         }
 
-        if ($is_cache) {
+        if ($isCache) {
 
-            return "<link rel='stylesheet' href='".$css_dir."/".$page_name.".css?v=".filemtime($cache_dir.'/'.$page_name.'.css')."'>";
+            return "<link rel='stylesheet' href='".$cssDir."/".$pageName.$hash.".css?v=".filemtime($cacheDir.'/'.$pageName.$hash.'.css')."'>";
         }
 
-        $style_files = [];
+        $styleFiles = [];
 
         foreach (['desktop', 'mobile'] as $device) {
             
-            $style_files[$device] = file_get_contents(self::$style_source_path[$device]);
+            $styleFiles[$device] = file_get_contents(self::$style_source_path[$device]);
 
-            foreach (self::$views[$device] as $view) {
+            if (isset(self::$views[$device])) {
 
-                $style_files[$device] .= ' ' . self::removeTags($view);
+                foreach (self::$views[$device] as $view) {
+
+                    $styleFiles[$device] .= ' ' . self::removeTags($view);
+                }
             }
         }
         
         $styles = '@media (min-width: 1600px) { ';
-        $styles .= $style_files['desktop'];
+        $styles .= $styleFiles['desktop'];
         $styles .= ' }';
 
         $styles .= '@media (max-width: 1600px) and (min-width: 1000px) { ';
-        $styles .= self::pxToVw($style_files['desktop'], self::$width['desktop']);
+        $styles .= self::pxToVw($styleFiles['desktop'], self::$width['desktop'], 'desktop');
         $styles .= ' }';
 
         $styles .= self::addTabletMedia(); 
         $styles .= '@media (max-width: 1000px) { ';
-        $styles .= self::pxToVw($style_files['mobile'], self::$width['mobile']);
+        $styles .= self::pxToVw($styleFiles['mobile'], self::$width['mobile'], 'mobile');
         $styles .= ' }';
 
-        file_put_contents($page_css, $styles);
-        touch($page_css, $cache_time ?? null);
+        file_put_contents($pageCss, $styles);
+        touch($pageCss, $cacheTime);
 
-        return "<link rel='stylesheet' href='".$css_dir."/".$page_name.".css?v=".filemtime($cache_dir.'/'.$page_name.'.css')."'>";
+        return "<link rel='stylesheet' href='".$cssDir."/".$pageName.$hash.".css?v=".filemtime($cacheDir.'/'.$pageName.$hash.'.css')."'>";
     }
 
     public static function removeTags($style)
-	{
+    {
         return str_replace(['</style>', '<style>'], ['',''], htmlspecialchars_decode($style));
     }
 
-    public static function pxToVw($styles, $width)
-	{
+    public static function pxToVw($styles, $width, $platform)
+    {
         preg_match_all('/[ :]{1}[-]{0,1}[0-9\.]+px/', $styles, $matches);
 
         for ($i = 0; $i < count($matches[0]); $i++) {
 
-            if ($matches[0][$i] == ' 1px' || $matches[0][$i] == ' 2px' || $matches[0][$i] == ' 3px' || $matches[0][$i] == ' -1px' || $matches[0][$i] == ' -2px' || $matches[0][$i] == ' -3px')
-                continue;
+            if ($matches[0][$i] == ' 1px' || 
+                $matches[0][$i] == ' 2px' || 
+                $matches[0][$i] == ' 3px' || 
+                $matches[0][$i] == ' -1px' || 
+                $matches[0][$i] == ' -2px' || 
+                $matches[0][$i] == ' -3px'){
+                    
+                    continue;
+            }
 
             $val = floatval($matches[0][$i]);
 
             $vw = round($val / $width * 100, 4);
 
-            if (Platform::mobile())
+            if ($platform == 'mobile'){
+
                 $styles = str_replace($matches[0][$i], ' calc('.$vw.'vw * var(--coef-w))', $styles);
-            else 
+            
+            } else {
+
                 $styles = str_replace($matches[0][$i], ' '.$vw.'vw', $styles);
+            } 
             
         }
 
@@ -162,11 +179,11 @@ class Convertor
 
 
     public static function addTabletMedia()
-	{
+    {
         return 
         '@media (min-width: calc(494px)) and (max-width: calc(1000px)) {
             body {
-                --coef-w: calc(var(--width) / -1310 + 1.3771);
+                --coef-w: calc(var(--width) / -910 + 1.3771);
                 --offset: calc(4.6875vw * (('.strval(self::$width['mobile']).' - '.strval(self::$width['mobile']).' * var(--coef-w) + 30 * var(--coef-w)) / 30));
                 --column-width: calc( (1px * var(--width)) - (2 * var(--offset)) );
             }
@@ -175,7 +192,7 @@ class Convertor
         @media (max-width: calc(494px)) {
             body {
                 --coef-w: 1;
-                --column-width: 87.2vw;
+                --column-width: 90.625vw;
                 --offset: calc(50% - (var(--column-width) / 2));
             }
         }
