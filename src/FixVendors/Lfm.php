6fb477a5177b3cf3714913643433cdf4f474e0ab
brightsvl <sv@digiants.com.ua>
2025-10-06 20:06:31 +0300

    fix lfm deprecated issue with install

--- src/FixVendors/Lfm.php
+++ src/FixVendors/Lfm.php
@@ -9,6 +9,12 @@ class Lfm
 {
     public function fix()
     {
+        $this->fixUploadController();
+        $this->fixLfmPath();
+    }
+
+    protected function fixUploadController()
+    {
         $path = base_path('vendor/unisharp/laravel-filemanager/src/Controllers/UploadController.php');
 
         $content = File::get($path);
@@ -16,6 +22,21 @@ class Lfm
         $fixedContent = Str::replace(
             '$response = count($error_bag) > 0 ? $error_bag : parent::$success_response;',
             '$response = count($error_bag) > 0 ? $error_bag : array(parent::$success_response);',
+            $content
+        );
+
+        File::put($path, $fixedContent);
+    }
+
+    protected function fixLfmPath()
+    {
+        $path = base_path('vendor/unisharp/laravel-filemanager/src/LfmPath.php');
+
+        $content = File::get($path);
+
+        $fixedContent = Str::replace(
+            'public function __construct(Lfm $lfm = null)',
+            'public function __construct(Lfm $lfm)',
             $content
         );
 
