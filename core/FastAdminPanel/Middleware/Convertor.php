cbfd4fd120d1165431ae744b8dd5ee6d5718aaab
digiants-agency <52928468+digiants-agency@users.noreply.github.com>
2026-01-30 12:32:58 +0200

    add conditional css convertor

--- core/FastAdminPanel/Middleware/Convertor.php
+++ core/FastAdminPanel/Middleware/Convertor.php
@@ -4,14 +4,27 @@ namespace App\FastAdminPanel\Middleware;
 
 class Convertor
 {
+    // TODO: change replace logic to view composer or blade directive
     public function handle($request, $next, $guard = null)
     {
         $response = $next($request);
 
         if ($response->status() !== 500) {
 
+            $content = $response->content();
+            $strpos = strpos($content, '%convertor%');
+
+            if ($strpos === false) {
+                return $response;
+            }
+
             $response->setContent(
-                str_replace('%convertor%', \App\FastAdminPanel\Helpers\Convertor::convert(), $response->content())
+                substr_replace(
+                    $content,
+                    \App\FastAdminPanel\Helpers\Convertor::convert(),
+                    $strpos,
+                    12
+                )
             );
         }
 
